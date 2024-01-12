689555a885457ada8b2fe96710c25919af64dd12
Vinnycius Melo <vinnycius.melo@gmail.com>
2024-01-12 04:11:10 -0300

    Melhorando o joguinho

--- jogo-soldados.py
+++ jogo-soldados.py
@@ -1,47 +1,85 @@
-import random
+from random import choice
+from time import sleep
 
-jogo = 'Jogo'
-print('{:^90}'.format(jogo))
-print('Caso a moeda caia com a face coroa, o soldado avança um território e se múltiplica')
-print('Caso a moeda caia com a face cara, o soldado morre')
-print('-' * 20)
+def resultado(cara=False):
+    print(f'\033[33mMoeda\033[m: {moeda}')
+    print('Soldado avança e múltiplica' if not cara else 'Soldado morre!')
+    print(f'Numero de soldados: {soldado}')
+    if cara:
+        print('\033[32m( ͡° ͜ʖ ͡°)\033[m' * soldado, end='')
+        print('\033[31m ×͜× \033[m')
+    else:
+        print('\033[32m( ͡° ͜ʖ ͡°)\033[m' * soldado)
+    print()
 
-print('Escolha seu time entre: Cara e Coroa')
-time = input('')
-print('')
+def tabela(cara=False):
+    if cara:
+        print('\033[31mTodos soldados mortos!\nTime Cara vecendor\033[m')
+    else:
+        print('\033[32mSoldados avançaram todo o território! \nTime mcoroa vencedor\033[m')
+    print('Número de rodadas | Vezes que caiu coroa | Vezes em que caiu cara | Total de gerações')
+    print(f'{rodadas:^17} | {coroa:^20} | {cara:^22} | {soldado:^18}')
 
-soldado = 1
-rodadas = 0
-cara = 0
-coroa = 0
-while soldado < 5:
-    lista = ['cara', 'coroa']
-    moeda = random.choice(lista)
+print('-=' * 45)
+print(f'{"Jogo da moeda":^90}')
+print('-=' * 45)
+print('''Caso a moeda caia com a face coroa, o soldado avança um território e se múltiplica
+Caso a moeda caia com a face cara, o soldado morre
+Time coroa vence quando tiver 6 soldados
+Time cara vence caso não haja nenhum soldado''')
+print('-' * 90)
 
-    if moeda == 'cara':
-        soldado -= 1
-        rodadas += 1
-        cara += 1
+print('Escolha entre \033[31mCara\033[m e \033[32mCoroa\033[m')
+time = str(input('\033[33mTime\033[m: '))
+jogos = int(input('Deseja jogar quantas partidas? '))
+sleep(1)
 
-        print('Moeda: {}\nSoldado morre!'.format(moeda))
-        print('Numero de soldados: {}'.format(soldado))
-        print('( ͡° ͜ʖ ͡°) ' * soldado, end=' ')
-        print('×͜×')
-        print('')
-        if soldado == 0:
-            print('Todos soldados mortos!\nTime cara vecendor')
-            break
+lista = ['cara', 'coroa']
+timeCoroa = timeCara = 0
+cont = 1
+while cont != jogos + 1:
+    soldado = 1
+    t = f'Jogo {cont}'
+    print()
+    print('-=' * 45)
+    print(f'{t:^90}')
+    print('-=' * 45)
+    while True:   
+        rodadas = cara = coroa = 0
+        moeda = choice(lista)
 
-    elif moeda == 'coroa':
-        soldado += 2
-        rodadas += 1
-        coroa += 1
-        print('Moeda: {}\nSoldado avança e múltiplica'.format(moeda))
-        print('Numero de soldados: {}'.format(soldado))
-        print('( ͡° ͜ʖ ͡°) ' * soldado)
-        print('')
-        if soldado == 6:
-            print('Time coroa vencedor')
+        if moeda == 'cara':
+            soldado -= 1
+            rodadas += 1
+            cara += 1
+            resultado(cara=True)
+            sleep(1)
+            if soldado <= 0:
+                timeCara += 1
+                tabela(cara=True)
+                sleep(2)
+                break
 
-print('Número de rodadas | Vezes que caiu coroa | Vezes em que caiu cara | Total de gerações')
-print('{:^17} | {:^20} | {:^22} | {:^18}'.format(rodadas, cara, coroa, soldado))
+        elif moeda == 'coroa':
+            soldado += 2
+            rodadas += 1
+            coroa += 1
+            resultado()
+            sleep(1)
+            if soldado >= 6:
+                timeCoroa += 1
+                tabela()
+                sleep(2)
+                break
+    cont += 1            
+
+print()
+print('-' * 90)
+print(f'Time cara venceu {timeCara}')
+print(f'Time coroa venceu {timeCoroa}')
+if timeCara > timeCoroa:
+    print('\033[31mTime cara vencedor\033[m')
+elif timeCara == timeCoroa:
+    print('\033[33mEmpate\033[m')
+else:
+    print('\033[32mTime coroa vencedor\033[m')
